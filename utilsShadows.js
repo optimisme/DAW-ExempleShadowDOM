184adc91d66213bf64e17633ee5e924f0ebdc027
optimisme <optimisme@gmail.com>
2023-10-26 14:02:33 +0200

    .

--- utilsShadows.js
+++ utilsShadows.js
@@ -1,6 +1,7 @@
 const fs = require('fs').promises
 const path = require('path');
 const glob = require('glob');
+const { match } = require('assert');
 
 class Obj {
 
@@ -19,65 +20,89 @@ class Obj {
             const jsFiles = await new Promise((resolve, reject) => {
                 glob('**/*.js', { cwd: shadowsPath }, (err, files) => {
                     if (err) {
-                        reject(err);
+                        reject(err)
                     } else {
-                        resolve(files);
+                        resolve(files)
                     }
                 });
-            });
+            })
     
-            const scripts = jsFiles.map(jsFile => `<script src="/shadows/${jsFile}" defer></script>`).join('\n        ');
-            let indexTXT = await fs.readFile(indexPath, 'utf8');
+            const scripts = jsFiles.map(jsFile => `<script src="/shadows/${jsFile}" defer></script>`).join('\n        ')
+            let indexTXT = await fs.readFile(indexPath, 'utf8')
             
             // Reemplaza la etiqueta de script existente con los nuevos scripts
-            indexTXT = indexTXT.replace('<script src="/shadows.js" defer></script>', scripts);
+            indexTXT = indexTXT.replace('<script src="/shadows.js" defer></script>', scripts)
     
             return indexTXT;
         } catch (error) {
-            console.error("Error 'processIndexDev':", error);
-            return "<html><body>Error</body></html>";
+            console.error("Error 'processIndexDev':", error)
+            return "<html><body>Error</body></html>"
         }
     }
     
-
-    async processShadows (shadowsPath) {
-        // Crea un arxiu 'shadows.js' amb tots els codis dels shadows
-        // inclosos els fitxers .css i .html corresponents
-        let accumulatedContent = ""
+    async processShadows(shadowsPath) {
+        let accumulatedContent = "";
         try {
-            const files = await fs.readdir(shadowsPath)
+            const files = await fs.readdir(shadowsPath);
+    
             for (const file of files) {
-            const fullPath = path.join(shadowsPath, file);
-            const stats = await fs.stat(fullPath);
-            if (stats.isFile() && path.extname(file) === '.js') {
-                let fileTXT = await fs.readFile(fullPath, 'utf8')
-                let fixed = fileTXT;
-        
-                // Substituir el contingut del .css
-                const cssFilenameMatch = fixed.match(/await fetch\('\/shadows\/(.*\.css)'\)/)
-                if (cssFilenameMatch) {
-                    const cssFilename = cssFilenameMatch[1]
-                    const cssContent = await fs.readFile(path.join(shadowsPath, cssFilename), 'utf8')
-                    fixed = fixed.replace(cssFilenameMatch[0], `\`\n${cssContent}\n\``).replace(".then(r => r.text())", "")
+                const fullPath = path.join(shadowsPath, file);
+                const stats = await fs.stat(fullPath);
+    
+                if (stats.isFile() && path.extname(file) === '.js') {
+                    let fileTXT = await fs.readFile(fullPath, 'utf8')
+                    let fixed = fileTXT
+    
+                    // Substituir el contingut del .css i .html
+                    fixed = await this.replaceFetchWithContent(fixed)
+    
+                    accumulatedContent += '\n\n' + fixed
+                } else if (stats.isDirectory()) {
+                    // Recursivamente procesar subdirectorios y concatenar el resultado
+                    const subfolderContent = await this.processShadows(fullPath)
+                    accumulatedContent += '\n\n' + subfolderContent
                 }
-            
-                // Substituir el contingut del .html
-                const htmlFilenameMatch = fixed.match(/await fetch\('\/shadows\/(.*\.html)'\)/)
-                if (htmlFilenameMatch) {
-                    const htmlFilename = htmlFilenameMatch[1];
-                    const htmlContent = await fs.readFile(path.join(shadowsPath, htmlFilename), 'utf8')
-                    fixed = fixed.replace(htmlFilenameMatch[0], `\`\n${htmlContent}\n\``).replace(".then(r => r.text())", "").replaceAll("`", "\`")
-                }
-                accumulatedContent += '\n\n' + fixed;
-            } else if (stats.isDirectory()) {
-                // Recursivitat per processar subdirectoris
-                accumulatedContent += '\n\n' + await this.processShadows(shadowsPath)
-            }
             }
         } catch (error) {
             console.error("Error 'processShadows':", error)
         }
         return accumulatedContent
+    }
+    
+    async replaceFetchWithContent(input) {
+        const regex = /\/(.*?)\.\w+/g
+        const lines = input.split('\n')
+        let lineNumber = 0
+        let result = ''
+        for (const line of lines) {
+            lineNumber++
+            const match = line.match(regex)
+            if (match) {
+                const filePath = './public' + match;
+                const fullPath = path.resolve(filePath)
+                if (await this.isFile(fullPath)) {
+                    const fileTxt = await fs.readFile(fullPath, 'utf8')
+                    if (match[0].indexOf(".css") >= 0) {
+                        result += 'style.textContent = `\n' + fileTxt + '`\n'
+                    }
+                    if (match[0].indexOf(".html") >= 0) {
+                        result += 'const htmlContent = `\n' + fileTxt + '`\n'
+                    }
+                }
+            } else {
+                result += line + '\n'
+            }
+        }
+        return result;
+    }
+
+    async isFile(filePath) {
+        try {
+            const stats = await fs.stat(filePath)
+            return stats.isFile()
+        } catch (err) {
+            return false
+        }
     }
 }
 
